77bd44c633837b44ef45802840f2fbc68a89fe60
Tirthesh Pawar <97823275+developerPawar@users.noreply.github.com>
2022-07-18 15:24:06 +0530

    Completed Tenzies Game

--- tenzies/src/App.js
+++ tenzies/src/App.js
@@ -1,19 +1,57 @@
 import React from "react";
 import Die from "./components/Die"
+import {nanoid} from "nanoid"
+import Confetti from 'react-confetti'
+
 export default function App(){
+
+  const [dice,setDice]=React.useState(allNewDice())
+  const[tenzies, setTenzies] = React.useState(false)
+
+  React.useEffect(()=>{
+    const eachHeld = dice.every(die => die.isHeld)
+    const firstValue = dice[0].value
+    const checkedValue = dice.every(die => die.value === firstValue)
+
+    if(eachHeld && checkedValue){
+      setTenzies(true)
+    }
+  },[dice])
   // Generates array of 10 random integers from 1-6
   // runs for loop 10 times pushes each random number at last of array 
   // .floor is used for only getting integers, .random * (1 - lowest value +  highest val) + lowest val
+  function generateNewDie(){
+    return {
+      value : Math.floor(Math.random() * (1 - 1 + 10)) + 1,
+      isHeld: false,
+      id: nanoid()
+    }
+  }
   function allNewDice(){
     const randNumArray = []
     for(let i=0;i<10;i++){
-      randNumArray.push(Math.floor(Math.random() * (1 - 1 + 10)) + 1 )
+      randNumArray.push(generateNewDie())
     }
     return randNumArray;
   }
-  const [dice,setDice]=React.useState(allNewDice())
+  function rollDice(){
+    if(!tenzies){
+      setDice(oldDice => oldDice.map(die =>{
+        return die.isHeld ? die :  generateNewDie()
+      }))
+    }else{
+      setTenzies(false)
+      setDice(allNewDice())
+    }
+  }
+  function holdDice(id){
+    setDice(oldDice => oldDice.map(die =>{
+        return die.id === id ? {...die, isHeld:!die.isHeld} : die
+    }))
+  }
+
   const diceElements = dice.map(die=>{
-    return <Die value={die} />
+    return <Die value={die.value} key={die.id} isHeld={die.isHeld} id={die.id} holdDice={() => holdDice(die.id)}/>
   })
 // Another method to generate array of 10 random numbers: 
 //   function allNewDice() {
@@ -26,9 +64,13 @@ export default function App(){
   console.log(allNewDice())
   return(
     <main>
+      {tenzies && <Confetti />}
+      <h1 className="title">Tenzies</h1>
+        <p className="instructions">Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
       <div className="die-holder">
         {diceElements}
       </div>
+      <button className="roll-dice" onClick={rollDice}>{tenzies ? "New Game" : "Roll"}</button>
     </main>
   )
 }
--- tenzies/src/components/Die.js
+++ tenzies/src/components/Die.js
@@ -1,8 +1,13 @@
 import React from "react";
 
 export default function Die(props) {
+    // Conditional rendering is applied using prop isHeld to check for the die condition and apply it to 'style'
+    const bgColor = { 
+        backgroundColor: props.isHeld ? "#59E391" : "white" 
+    }
+
     return (
-        <div className="die-styling">
+        <div className="die-styling" style={bgColor} onClick={props.holdDice}>
              <h2 className="die-num">{props.value}</h2>
         </div>
     )
